4345d9f75f6223a8fa5b263538e4592036bc7b0b
devinleighsmith <devinleighsmith@gmail.com>
2024-08-29 08:50:37 -0700

    correct queues.py syntax

--- mayan/apps/dynamic_search/queues.py
+++ mayan/apps/dynamic_search/queues.py
@@ -52,5 +52,5 @@ queue_search.add_task_type(
     dotted_path='mayan.apps.dynamic_search.tasks.task_saved_resultset_expired_delete',
     label=_(message='Delete expired saved resultsets'),
     name='task_saved_resultset_expired_delete',
-    schedule=(timedelta(seconds=TASK_SAVED_RESULTSET_EXPIRED_DELETE_INTERVAL if not setting_search_disable.value else None))
+    schedule=( timedelta(seconds=TASK_SAVED_RESULTSET_EXPIRED_DELETE_INTERVAL) if not setting_search_disable.value else None)
 )
