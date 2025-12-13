4878be3d1c19e14caf67d8d4faaaaa627e50bc50
Roberto Rosario <roberto.rosario@mayan-edms.com>
2025-12-13 02:40:18 -0400

    Make several class label and text translatable
    - Make document file search fields translatable.
    - Make tag document list link title translatable.
    - Make the task worker labels translatable.
    
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/task_manager/workers.py
+++ mayan/apps/task_manager/workers.py
@@ -18,21 +18,23 @@ from .classes import Worker
 worker_a = Worker(
     concurrency=MAYAN_WORKER_A_CONCURRENCY,
     description=_(message='Low latency high volume tasks'),
-    label=('Worker A'),
+    label=_(message='Worker A'),
     maximum_memory_per_child=MAYAN_WORKER_A_MAX_MEMORY_PER_CHILD,
     maximum_tasks_per_child=MAYAN_WORKER_A_MAX_TASKS_PER_CHILD,
     name='worker_a', nice_level=MAYAN_WORKER_A_NICE_LEVEL
 )
 worker_b = Worker(
     concurrency=MAYAN_WORKER_B_CONCURRENCY,
-    description=_(message='Medium latency tasks'), label=('Worker B'),
+    description=_(message='Medium latency tasks'),
+    label=_(message='Worker B'),
     maximum_memory_per_child=MAYAN_WORKER_B_MAX_MEMORY_PER_CHILD,
     maximum_tasks_per_child=MAYAN_WORKER_B_MAX_TASKS_PER_CHILD,
     name='worker_b', nice_level=MAYAN_WORKER_B_NICE_LEVEL
 )
 worker_c = Worker(
     concurrency=MAYAN_WORKER_C_CONCURRENCY,
-    description=_(message='Medium latency tasks'), label=('Worker C'),
+    description=_(message='Medium latency tasks'),
+    label=_(message='Worker C'),
     maximum_memory_per_child=MAYAN_WORKER_C_MAX_MEMORY_PER_CHILD,
     maximum_tasks_per_child=MAYAN_WORKER_C_MAX_TASKS_PER_CHILD,
     name='worker_c', nice_level=MAYAN_WORKER_C_NICE_LEVEL
@@ -40,7 +42,7 @@ worker_c = Worker(
 worker_d = Worker(
     concurrency=MAYAN_WORKER_D_CONCURRENCY,
     description=_(message='Low latency, long lived tasks'),
-    label=('Worker D'),
+    label=_(message='Worker D'),
     maximum_memory_per_child=MAYAN_WORKER_D_MAX_MEMORY_PER_CHILD,
     maximum_tasks_per_child=MAYAN_WORKER_D_MAX_TASKS_PER_CHILD,
     name='worker_d', nice_level=MAYAN_WORKER_D_NICE_LEVEL
@@ -48,7 +50,7 @@ worker_d = Worker(
 worker_e = Worker(
     concurrency=MAYAN_WORKER_E_CONCURRENCY,
     description=_(message='Low latency, long lived tasks'),
-    label=('Worker E'),
+    label=_(message='Worker E'),
     maximum_memory_per_child=MAYAN_WORKER_E_MAX_MEMORY_PER_CHILD,
     maximum_tasks_per_child=MAYAN_WORKER_E_MAX_TASKS_PER_CHILD,
     name='worker_e', nice_level=MAYAN_WORKER_E_NICE_LEVEL
