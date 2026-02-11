3b03960e6a4245e2786d72a0e6a608d77a92ede1
Roberto Rosario <roberto.rosario@mayan-edms.com>
2026-02-11 16:55:25 -0400

    Code style updates
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/platforms/settings.py
+++ mayan/apps/platforms/settings.py
@@ -20,23 +20,24 @@ setting_namespace = setting_cluster.do_namespace_add(
 setting_client_backend_enabled = setting_namespace.do_setting_add(
     default=DEFAULT_PLATFORMS_CLIENT_BACKEND_ENABLED,
     global_name='PLATFORMS_CLIENT_BACKEND_ENABLED', help_text=_(
-        message='List of client backends to launch after startup. Use full dotted '
-        'path to the client backend classes.'
+        message='List of client backends to launch after startup. Use full '
+        'dotted path to the client backend classes.'
     )
 )
 setting_client_backend_arguments = setting_namespace.do_setting_add(
     default=DEFAULT_PLATFORMS_CLIENT_BACKEND_ARGUMENTS,
     global_name='PLATFORMS_CLIENT_BACKEND_ARGUMENTS', help_text=_(
-        message='Arguments for the client backends. Use the client backend dotted '
-        'path as the dictionary key for the arguments in dictionary format.'
+        message='Arguments for the client backends. Use the client backend '
+        'dotted path as the dictionary key for the arguments in dictionary '
+        'format.'
     )
 )
 
 # Supervisord
 
 setting_namespace.do_setting_add(
-    default=DEFAULT_SETTINGS_MODULE,
-    global_name='MAYAN_SETTINGS_MODULE', help_text=_(
+    default=DEFAULT_SETTINGS_MODULE, global_name='MAYAN_SETTINGS_MODULE',
+    help_text=_(
         message='Load an alternate settings file.'
     )
 )
@@ -44,22 +45,15 @@ setting_namespace.do_setting_add(
 # Gunicorn
 
 setting_namespace.do_setting_add(
-    default=GUNICORN_WORKERS,
-    global_name='MAYAN_GUNICORN_WORKERS', help_text=_(
-        message='Allows setting Gunicorn\'s `workers` value.'
+    default=GUNICORN_LIMIT_REQUEST_LINE,
+    global_name='MAYAN_GUNICORN_LIMIT_REQUEST_LINE', help_text=_(
+        message='Allows setting Gunicorn\'s `limit_request_line` value.'
     )
 )
 setting_namespace.do_setting_add(
-    default=GUNICORN_WORKER_CLASS,
-    global_name='MAYAN_GUNICORN_WORKER_CLASS', help_text=_(
-        message='Allows setting Gunicorn\'s `worker_class` value.'
-
-    )
-)
-setting_namespace.do_setting_add(
-    default=GUNICORN_TIMEOUT,
-    global_name='MAYAN_GUNICORN_TIMEOUT', help_text=_(
-        message='Allows setting Gunicorn\'s `timeout` value.'
+    default=GUNICORN_MAX_REQUESTS, global_name='MAYAN_GUNICORN_MAX_REQUESTS',
+    help_text=_(
+        message='Allows setting Gunicorn\'s `max_requests` value.'
     )
 )
 setting_namespace.do_setting_add(
@@ -69,14 +63,21 @@ setting_namespace.do_setting_add(
     )
 )
 setting_namespace.do_setting_add(
-    default=GUNICORN_MAX_REQUESTS,
-    global_name='MAYAN_GUNICORN_MAX_REQUESTS', help_text=_(
-        message='Allows setting Gunicorn\'s `max_requests` value.'
+    default=GUNICORN_TIMEOUT, global_name='MAYAN_GUNICORN_TIMEOUT',
+    help_text=_(
+        message='Allows setting Gunicorn\'s `timeout` value.'
     )
 )
 setting_namespace.do_setting_add(
-    default=GUNICORN_LIMIT_REQUEST_LINE,
-    global_name='MAYAN_GUNICORN_LIMIT_REQUEST_LINE', help_text=_(
-        message='Allows setting Gunicorn\'s `limit_request_line` value.'
+    default=GUNICORN_WORKER_CLASS, global_name='MAYAN_GUNICORN_WORKER_CLASS',
+    help_text=_(
+        message='Allows setting Gunicorn\'s `worker_class` value.'
+
+    )
+)
+setting_namespace.do_setting_add(
+    default=GUNICORN_WORKERS, global_name='MAYAN_GUNICORN_WORKERS',
+    help_text=_(
+        message='Allows setting Gunicorn\'s `workers` value.'
     )
 )
