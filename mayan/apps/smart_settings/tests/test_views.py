d209b99703181194971ced90dc43caf7ab39e1c0
Roberto Rosario <roberto.rosario@mayan-edms.com>
2026-02-18 17:15:27 -0400

    Add setting data type view test
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/smart_settings/tests/test_views.py
+++ mayan/apps/smart_settings/tests/test_views.py
@@ -26,6 +26,179 @@ class SettingClusterViewTestCase(
         self.assertEqual(response.status_code, 302)
 
 
+class SettingDataTypeViewTestCase(SettingViewTestMixin, GenericViewTestCase):
+    _test_setting_namespace_create_auto = True
+
+    def test_setting_type_all_dictionary_string_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value="{'a': [False, '11', 11, ['x', 'y', {'b': 99}]]}"
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(),
+            {'a': [False, '11', 11, ['x', 'y', {'b': 99}]]}
+        )
+
+    def test_setting_type_boolean_dictionary_string_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value="{'a': 1}"
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(), {'a': 1}
+        )
+
+    def test_setting_type_boolean_false_native_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value=False
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(), False
+        )
+
+    def test_setting_type_boolean_false_string_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value='False'
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(), False
+        )
+
+    def test_setting_type_boolean_true_native_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value=True
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(), True
+        )
+
+    def test_setting_type_boolean_true_string_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value='True'
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(), True
+        )
+
+    def test_setting_type_boolean_integer_native_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value=99
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(), 99
+        )
+
+    def test_setting_type_boolean_integer_string_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value='99'
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(), 99
+        )
+
+    def test_setting_type_float_native_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value=1.2
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(), 1.2
+        )
+
+    def test_setting_type_float_string_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value='1.2'
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(), 1.2
+        )
+
+    def test_setting_type_list_string_block_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value='- a\n- b'
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(), ['a', 'b']
+        )
+
+    def test_setting_type_list_string_inline_edit_view_with_permission(self):
+        self.grant_permission(permission=permission_settings_edit)
+
+        self._create_test_setting()
+
+        response = self._request_setting_edit_view(
+            value='["a", "b"]'
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(
+            self._test_setting.get_value_pending(), ['a', 'b']
+        )
+
+
 class SettingNamespaceViewTestCase(
     SettingNamespaceViewTestMixin, GenericViewTestCase
 ):
